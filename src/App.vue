e58fb50041f87a183688cca7e45dff2a4a7c2563
Andre Butler <andrebutler@Andres-MacBook-Air.local>
2020-10-02 15:45:06 -0500

    first commit with static.json

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <label>
+      <h1>Andre's First vue2-google-maps Demo for
+        <a href="https://indagodigital.us/">Indago Digital, Inc.</a>
+      </h1>
+      <hr>
+      <GmapAutocomplete @place_changed="setPlace">
+      </GmapAutocomplete>
+      <button @click="usePlace">Add</button>
+    </label>
+    <br/>
+
+    <GmapMap style="width: 600px; height: 300px;" :zoom="1" :center="{lat: 0, lng: 0}">
+      <GmapMarker v-for="(marker, index) in markers"
+        :key="index"
+        :position="marker.position"
+        />
+      <GmapMarker
+        v-if="this.place"
+        label="★"
+        :position="{
+          lat: this.place.geometry.location.lat(),
+          lng: this.place.geometry.location.lng(),
+        }"
+        />
+    </GmapMap>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      markers: [],
+      place: null
+    }
+  },
+  description: 'Autocomplete Example (#164)',
+  methods: {
+    setDescription (description) {
+      this.description = description
+    },
+    setPlace (place) {
+      this.place = place
+    },
+    usePlace (place) {
+      if (this.place) {
+        this.markers.push({
+          position: {
+            lat: this.place.geometry.location.lat(),
+            lng: this.place.geometry.location.lng()
+          }
+        })
+        this.place = null
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB1-ikCnOtwWPF9nyMZi2B0FA9ISDjLZ34'
+  }
+})
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
